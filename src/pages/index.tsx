59698cbe6f3d750af5d0fa9cbb0fee5147686370
ardotheedu <eduardosalima3@gmail.com>
2021-05-07 23:28:10 -0300

    feat: posts index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +30,62 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <main className={styles.container}>
+      <div className={styles.posts}>
+        {postsPagination.results.map(post => (
+          <Link href={`/posts/${post.uid}`}>
+            <a key={post.uid}>
+              <strong>{post.data.title}</strong>
+              <p>{post.data.subtitle}</p>
+              <div>
+                <div>
+                  <time>{post.first_publication_date}</time>
+                </div>
+                <div>
+                  <p>{post.data.subtitle}</p>
+                </div>
+              </div>
+            </a>
+          </Link>
+        ))}
+      </div>
+      <a key={postsPagination.next_page}>carregar mais posts</a>
+    </main>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content'],
+      pageSize: 100,
+    }
+  );
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(new Date(), post.first_publication_date, {
+        locale: ptBR,
+      }),
+      data: {
+        title: RichText.asText(post.data.title),
+        subtitle: RichText.asText(post.data.subtitle),
+        author: RichText.asText(post.data.author),
+      },
+    };
+  });
+
+  const { next_page } = postsResponse;
+
+  return {
+    props: {
+      posts,
+      next_page,
+    },
+  };
+};
